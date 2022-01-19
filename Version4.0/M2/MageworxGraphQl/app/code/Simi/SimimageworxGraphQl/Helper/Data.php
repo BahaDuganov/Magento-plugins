046c28c3faf2898fa49d452bf170f5f2086cbc54
Cody Nguyen <cody@simicart.com>
2022-01-19 09:04:05 +0700

    Fix helper

--- Version4.0/M2/MageworxGraphQl/app/code/Simi/SimimageworxGraphQl/Helper/Data.php
+++ Version4.0/M2/MageworxGraphQl/app/code/Simi/SimimageworxGraphQl/Helper/Data.php
@@ -8,23 +8,28 @@ namespace Simi\SimimageworxGraphQl\Helper;
 
 use Magento\Framework\App\Filesystem\DirectoryList;
 use Mirasvit\SearchAutocomplete\Model\ConfigProvider as MirasvitSearchConfig;
+use Magento\Store\Model\StoreManagerInterface;
 
 class Data extends \Magento\Framework\App\Helper\AbstractHelper {
-	private $serializer;
-	protected $collectionPromotion;
-	protected $dateTime;
-	protected $catalogCollectionFactory;
+	
+	public $serializer;
+	public $collectionPromotion;
+	public $dateTime;
+	public $catalogCollectionFactory;
+	public $storeManager;
 
     public function __construct(
         \Magento\Framework\App\Helper\Context $context,
         \Magento\Framework\ObjectManagerInterface $simiObjectManager,
 	    \Magento\Framework\Serialize\SerializerInterface $serializer,
 		\Magento\Framework\Stdlib\DateTime\DateTime $dateTime,
+        StoreManagerInterface $storeManager,
 		\Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $catalogCollectionFactory
     ){
 		$this->serializer = $serializer;
 		$this->dateTime = $dateTime;
 		$this->catalogCollectionFactory = $catalogCollectionFactory->create();
+		$this->storeManager = $storeManager;
 		parent::__construct($context);
 	}
 
